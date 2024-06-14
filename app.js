74003c25b2119815e8010e8b98ec97b06b0e97a1
Jhean Carlo <jheanquintana905@gmail.com>
2024-06-14 16:58:23 -0500

    nodos

---
+++ app.js
@@ -0,0 +1,35 @@
+let win = window
+let dom = document
+let body = dom.body
+let divs = dom.querySelectorAll('div')
+let div = dom.querySelectorAll('div')
+let form = dom.forms 
+
+let poridnuevo = dom.querySelector('#documento')
+let porclase = dom.querySelector('container')
+let proclasell = dom.querySelectorAll('.container')
+let porid = dom.getElementById('documento')
+
+let atributo = dom.getElementsByName('search')
+
+console.log(win)
+console.log(dom)
+console.log(dom.doctype)
+console.log(document.head)
+console.log(dom.body)
+console.log(dom.body.children)
+console.log(dom.body [1])
+console.log(divs)
+console.log(div)
+console.log(form)
+console.log(dom.links)
+console.log(dom.images)
+console.log(porid)
+console.log(poridnuevo)
+console.log(porclase)
+console.log(proclasell)
+console.log(atributo)
+
+const listaA = [...body.children]
+console.log(listaA)
+
